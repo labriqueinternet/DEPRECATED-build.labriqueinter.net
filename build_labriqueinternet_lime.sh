9daa6f965ff0812f0024266a4c02bce48fc44a76
Émile Morel <github@bleuchtang.fr>
2015-07-24 11:40:16 +0200

    add kernel testing lime2

--- build_labriqueinternet_lime.sh
+++ build_labriqueinternet_lime.sh
@@ -18,21 +18,25 @@ cd /opt/sunxi-debian && git pull
  -t /srv/olinux/debootstrap -p -y | tee /srv/olinux/debootstrap.log
 
 cp /srv/olinux/sunxi.log /srv/olinux/debootstrap.log /srv/olinux/debootstrap/root/
-# partitioning doesn't work with losetup on my board...
-#/opt/sunxi-debian/olinux/create_device.sh -d img -s 1200 \
-# -t /srv/olinux/yunohost_lime.img -b /srv/olinux/debootstrap
 
-# Lime1 archive
-tar --same-owner --preserve-permissions -cvf \
- /srv/olinux/labriqueinternet_lime1_"$(date '+%d-%m-%Y')".tar \
- -C /srv/olinux/debootstrap .
+/opt/sunxi-debian/olinux/create_device.sh -d img -s 1400 \
+ -t /srv/olinux/labriqueinternet_lime1_"$(date '+%d-%m-%Y')" -b /srv/olinux/debootstrap
 
-# Lime2 archive (change symlink) 
-cd /srv/olinux/debootstrap && rm boot/board.dtb &&
-	ln -s boot/dtb/sun7i-a20-olinuxino-lime2.dtb boot/board.dtb
-# Lime2 archive
-tar --same-owner --preserve-permissions -cvf \
- /srv/olinux/labriqueinternet_lime2_"$(date '+%d-%m-%Y')".tar \
- -C /srv/olinux/debootstrap .
+cd /srv/olinux/debootstrap
+#search if kernel was build or was installed with debian testing repo
+if [ -f boot/board.dtb ] ; then
+  # Lime2 archive (change symlink) 
+  rm boot/board.dtb
+  ln -s boot/dtb/sun7i-a20-olinuxino-lime2.dtb boot/board.dtb
+else
+  kernel=$(ls -l | grep initrd | cut -d '-' -f3,4,5)
+  rm -f boot/dtb boot/dtb-$kernel boot/dtbs/$kernel/*
+  cp  usr/lib/linux-image-$kernel/sun7i-a20-olinuxino-lime2.dtb boot/dtbs/$kernel/
+  ln -s boot/dtbs/$kernel/sun7i-a20-olinuxino-lime2.dtb boot/dtb
+  ln -s boot/dtbs/$kernel/sun7i-a20-olinuxino-lime2.dtb boot/dtb-$kernel
+  echo Olimex A20-OLinuXino-LIME2 > etc/flash-kernel/machine
+fi
 
+/opt/sunxi-debian/olinux/create_device.sh -d img -s 1400 \
+ -t /srv/olinux/labriqueinternet_lime2_"$(date '+%d-%m-%Y')" -b /srv/olinux/debootstrap
 
