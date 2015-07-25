c811d93780089e80f41f617d9d73d812d0a08d36
Émile Morel <github@bleuchtang.fr>
2015-07-25 09:03:16 +0200

    add u-boot file from flash-kernel for testing kernel

--- build_labriqueinternet_lime.sh
+++ build_labriqueinternet_lime.sh
@@ -43,33 +43,35 @@ if [ $KERNEL_MODE = "compil" ] ; then
    -t /srv/olinux/sunxi -s | tee /srv/olinux/sunxi.log
   /opt/sunxi-debian/olinux/create_arm_debootstrap.sh -i /srv/olinux/sunxi/ \
    -t /srv/olinux/debootstrap -p -y | tee /srv/olinux/debootstrap.log
-else
-   /opt/sunxi-debian/olinux/create_arm_debootstrap.sh -i testing \
-   -t /srv/olinux/debootstrap -p -y | tee /srv/olinux/debootstrap.log
-fi
-
-if [ $KERNEL_MODE = "compil" ] ; then
   cp /srv/olinux/sunxi.log /srv/olinux/debootstrap.log /srv/olinux/debootstrap/root/
-else
-  cp /srv/olinux/debootstrap.log /srv/olinux/debootstrap/root/
-fi
-
-/opt/sunxi-debian/olinux/create_device.sh -d img -s 1400 \
- -t /srv/olinux/labriqueinternet_lime1_"$(date '+%d-%m-%Y')".img -b /srv/olinux/debootstrap
-
-cd /srv/olinux/debootstrap
-if [ $KERNEL_MODE = "compil" ] ; then
+  /opt/sunxi-debian/olinux/create_device.sh -d img -s 1400 \
+   -t /srv/olinux/labriqueinternet_lime1_"$(date '+%d-%m-%Y')".img \
+   -b /srv/olinux/debootstrap
+  cd /srv/olinux/debootstrap
   rm boot/board.dtb
   ln -s boot/dtb/sun7i-a20-olinuxino-lime2.dtb boot/board.dtb
+  /opt/sunxi-debian/olinux/create_device.sh -d img -s 1400 \
+   -t /srv/olinux/labriqueinternet_lime2_"$(date '+%d-%m-%Y')".img \
+   -b /srv/olinux/debootstrap
 else
+  /opt/sunxi-debian/olinux/create_arm_debootstrap.sh -i testing \
+   -t /srv/olinux/debootstrap -p -y | tee /srv/olinux/debootstrap.log
+  cp /srv/olinux/debootstrap.log /srv/olinux/debootstrap/root/
+  # Build olimex lime.img
+  /opt/sunxi-debian/olinux/create_device.sh -d img -s 1400 \
+   -t /srv/olinux/labriqueinternet_lime1_"$(date '+%d-%m-%Y')".img \
+   -b /srv/olinux/debootstrap \
+   -u /srv/olinux/debootstrap/usr/lib/u-boot/A20-OLinuXino-Lime/u-boot-sunxi-with-spl.bin 
+  # Build olime lime2 .img
+  cd /srv/olinux/debootstrap
   kernel=$(ls -l | grep initrd | cut -d '-' -f3,4,5)
   rm -f boot/dtb boot/dtb-$kernel boot/dtbs/$kernel/*
   cp  usr/lib/linux-image-$kernel/sun7i-a20-olinuxino-lime2.dtb boot/dtbs/$kernel/
   ln -s boot/dtbs/$kernel/sun7i-a20-olinuxino-lime2.dtb boot/dtb
   ln -s boot/dtbs/$kernel/sun7i-a20-olinuxino-lime2.dtb boot/dtb-$kernel
   echo Olimex A20-OLinuXino-LIME2 > etc/flash-kernel/machine
+  /opt/sunxi-debian/olinux/create_device.sh -d img -s 1400 \
+   -t /srv/olinux/labriqueinternet_lime2_"$(date '+%d-%m-%Y')".img \
+   -b /srv/olinux/debootstrap \
+   -u /srv/olinux/debootstrap/usr/lib/u-boot/A20-OLinuXino-Lime2/u-boot-sunxi-with-spl.bin 
 fi
-
-/opt/sunxi-debian/olinux/create_device.sh -d img -s 1400 \
- -t /srv/olinux/labriqueinternet_lime2_"$(date '+%d-%m-%Y')".img -b /srv/olinux/debootstrap
-
