ab0f45b88c5f2045ac4b0f59e43259ac4d63bc8b
Émile Morel <github@bleuchtang.fr>
2015-08-15 17:57:12 +0200

    add crypted img

--- build_labriqueinternet_lime.sh
+++ build_labriqueinternet_lime.sh
@@ -54,24 +54,43 @@ if [ $KERNEL_MODE = "compil" ] ; then
    -t /srv/olinux/labriqueinternet_lime2_"$(date '+%d-%m-%Y')".img \
    -b /srv/olinux/debootstrap
 else
+  # Build olinux debootstrap with yunohost
   /opt/sunxi-debian/olinux/create_arm_debootstrap.sh -i testing \
-   -t /srv/olinux/debootstrap -p -y | tee /srv/olinux/debootstrap.log
+   -t /srv/olinux/debootstrap -p -y -e | tee /srv/olinux/debootstrap.log
   cp /srv/olinux/debootstrap.log /srv/olinux/debootstrap/root/
-  # Build olimex lime.img
+
+  # Create .img for Lime
   /opt/sunxi-debian/olinux/create_device.sh -d img -s 1400 \
-   -t /srv/olinux/labriqueinternet_lime1_"$(date '+%d-%m-%Y')".img \
+   -t /srv/olinux/labriqueinternet_lime1_cryptedroot_"$(date '+%d-%m-%Y')".img \
    -b /srv/olinux/debootstrap \
    -u /srv/olinux/debootstrap/usr/lib/u-boot/A20-OLinuXino-Lime/u-boot-sunxi-with-spl.bin 
-  # Build olime lime2 .img
-  cd /srv/olinux/debootstrap
-  kernel=$(ls -l | grep initrd | cut -d '-' -f3,4,5)
-  rm -f boot/dtb boot/dtb-$kernel boot/dtbs/$kernel/*
-  cp  usr/lib/linux-image-$kernel/sun7i-a20-olinuxino-lime2.dtb boot/dtbs/$kernel/
-  ln -s boot/dtbs/$kernel/sun7i-a20-olinuxino-lime2.dtb boot/dtb
-  ln -s boot/dtbs/$kernel/sun7i-a20-olinuxino-lime2.dtb boot/dtb-$kernel
-  echo Olimex A20-OLinuXino-LIME2 > etc/flash-kernel/machine
+
+  # Create .img for Lime2
+  echo Olimex A20-OLinuXino-LIME2 > /srv/olinux/debootstrap/etc/flash-kernel/machine
+  chroot /srv/olinux/debootstrap 'update-initramfs -u -k all'
+  /opt/sunxi-debian/olinux/create_device.sh -d img -s 1400 \
+   -t /srv/olinux/labriqueinternet_lime2_cryptedroot_"$(date '+%d-%m-%Y')".img \
+   -b /srv/olinux/debootstrap \
+   -u /srv/olinux/debootstrap/usr/lib/u-boot/A20-OLinuXino-Lime2/u-boot-sunxi-with-spl.bin 
+
+   # Switch to unencrypted root
+  echo 'LINUX_KERNEL_CMDLINE="console=tty0 hdmi.audio=EDID:0 disp.screen0_output_mode=EDID:1280x720p60 root=/dev/mmcblk0p1 rootwait sunxi_ve_mem_reserve=0 sunxi_g2d_mem_reserve=0 sunxi_no_mali_mem_reserve sunxi_fb_mem_reserve=0 panic=10 loglevel=6 consoleblank=0"' > $TARGET_DIR/etc/default/flash-kernel
+  rm /srv/olinux/debootstrap/etc/crypttab
+  echo '/dev/mmcblk0p1      /	ext4    defaults        0       1' > /srv/olinux/debootstrap/etc/fstab
+
+  # Create .img for Lime2 without crypted root
+  chroot /srv/olinux/debootstrap 'update-initramfs -u -k all'
   /opt/sunxi-debian/olinux/create_device.sh -d img -s 1400 \
    -t /srv/olinux/labriqueinternet_lime2_"$(date '+%d-%m-%Y')".img \
    -b /srv/olinux/debootstrap \
    -u /srv/olinux/debootstrap/usr/lib/u-boot/A20-OLinuXino-Lime2/u-boot-sunxi-with-spl.bin 
+
+  # Create .img for Lime without crypted root
+  echo Olimex A20-OLinuXino-LIME > /srv/olinux/debootstrap/etc/flash-kernel/machine
+  chroot /srv/olinux/debootstrap 'update-initramfs -u -k all'
+  /opt/sunxi-debian/olinux/create_device.sh -d img -s 1400 \
+   -t /srv/olinux/labriqueinternet_lime1_cryptedroot_"$(date '+%d-%m-%Y')".img \
+   -b /srv/olinux/debootstrap \
+   -u /srv/olinux/debootstrap/usr/lib/u-boot/A20-OLinuXino-Lime/u-boot-sunxi-with-spl.bin 
 fi
+
