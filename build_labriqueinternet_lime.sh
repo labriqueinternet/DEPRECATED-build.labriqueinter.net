a998928c2df2df1abc9bb2933c84b6feb390eb97
Émile Morel <github@bleuchtang.fr>
2015-09-08 12:28:20 +0200

    add bananpi

--- build_labriqueinternet_lime.sh
+++ build_labriqueinternet_lime.sh
@@ -19,38 +19,34 @@ cd /opt/sunxi-debian && git pull
  -t /srv/olinux/debootstrap -p -y -e | tee /srv/olinux/debootstrap.log
 cp /srv/olinux/debootstrap.log /srv/olinux/debootstrap/root/
 
-# Create .img for Lime
-/opt/sunxi-debian/olinux/create_device.sh -d img -s 1400 \
- -t /srv/olinux/labriqueinternet_lime1_cryptedroot_"$(date '+%d-%m-%Y')".img \
- -b /srv/olinux/debootstrap \
- -u /srv/olinux/debootstrap/usr/lib/u-boot/A20-OLinuXino-Lime/u-boot-sunxi-with-spl.bin 
+board=( 'Olimex A20-OLinuXino-LIME' 'Olimex A20-OLinuXino-LIME2' 'LeMaker Banana Pi' )
+uboot=( 'A20-OLinuXino-Lime' 'A20-OLinuXino-Lime2' 'Bananapi' )
 
-# Create .img for Lime2
-echo Olimex A20-OLinuXino-LIME2 > /srv/olinux/debootstrap/etc/flash-kernel/machine
-chroot_deb /srv/olinux/debootstrap 'update-initramfs -u -k all'
-/opt/sunxi-debian/olinux/create_device.sh -d img -s 1400 \
- -t /srv/olinux/labriqueinternet_lime2_cryptedroot_"$(date '+%d-%m-%Y')".img \
- -b /srv/olinux/debootstrap \
- -u /srv/olinux/debootstrap/usr/lib/u-boot/A20-OLinuXino-Lime2/u-boot-sunxi-with-spl.bin 
+for i in `seq 0 ${#board[@]}`; do 
+
+  echo ${board[$i]} > /srv/olinux/debootstrap/etc/flash-kernel/machine
+  chroot_deb /srv/olinux/debootstrap 'update-initramfs -u -k all'
+  /opt/sunxi-debian/olinux/create_device.sh -d img -s 1400 \
+   -t /srv/olinux/labriqueinternet_${uboot[$i]}_"$(date '+%d-%m-%Y')".img \
+   -b /srv/olinux/debootstrap \
+   -u /srv/olinux/debootstrap/usr/lib/u-boot/${uboot[$i]}/u-boot-sunxi-with-spl.bin 
+  
+done
 
  # Switch to unencrypted root
 echo 'LINUX_KERNEL_CMDLINE="console=tty0 hdmi.audio=EDID:0 disp.screen0_output_mode=EDID:1280x720p60 root=/dev/mmcblk0p1 rootwait sunxi_ve_mem_reserve=0 sunxi_g2d_mem_reserve=0 sunxi_no_mali_mem_reserve sunxi_fb_mem_reserve=0 panic=10 loglevel=6 consoleblank=0"' > $TARGET_DIR/etc/default/flash-kernel
 rm /srv/olinux/debootstrap/etc/crypttab
 echo '/dev/mmcblk0p1      /	ext4    defaults        0       1' > /srv/olinux/debootstrap/etc/fstab
+  
+for i in `seq 0 ${#board[@]}`; do 
 
-# Create .img for Lime2 without crypted root
-chroot_deb /srv/olinux/debootstrap 'update-initramfs -u -k all'
-/opt/sunxi-debian/olinux/create_device.sh -d img -s 1400 \
- -t /srv/olinux/labriqueinternet_lime2_"$(date '+%d-%m-%Y')".img \
- -b /srv/olinux/debootstrap \
- -u /srv/olinux/debootstrap/usr/lib/u-boot/A20-OLinuXino-Lime2/u-boot-sunxi-with-spl.bin 
-
-# Create .img for Lime without crypted root
-echo Olimex A20-OLinuXino-LIME > /srv/olinux/debootstrap/etc/flash-kernel/machine
-chroot_deb /srv/olinux/debootstrap 'update-initramfs -u -k all'
-/opt/sunxi-debian/olinux/create_device.sh -d img -s 1400 \
- -t /srv/olinux/labriqueinternet_lime1_"$(date '+%d-%m-%Y')".img \
- -b /srv/olinux/debootstrap \
- -u /srv/olinux/debootstrap/usr/lib/u-boot/A20-OLinuXino-Lime/u-boot-sunxi-with-spl.bin 
+  echo ${board[$i]} > /srv/olinux/debootstrap/etc/flash-kernel/machine
+  chroot_deb /srv/olinux/debootstrap 'update-initramfs -u -k all'
+  /opt/sunxi-debian/olinux/create_device.sh -d img -s 1400 \
+   -t /srv/olinux/labriqueinternet_${uboot[$i]}_cryptedroot_"$(date '+%d-%m-%Y')".img \
+   -b /srv/olinux/debootstrap \
+   -u /srv/olinux/debootstrap/usr/lib/u-boot/${uboot[$i]}/u-boot-sunxi-with-spl.bin 
+  
+done
 
 exit 0
