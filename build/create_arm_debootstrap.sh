df6334b13b62103d969de4fc8d970ee81bf79c3c
Julien Vaubourg <julien@vaubourg.com>
2016-05-02 22:36:22 +0200

    Secure the status code of rmdir

--- build/create_arm_debootstrap.sh
+++ build/create_arm_debootstrap.sh
@@ -241,7 +241,7 @@ if [ $INSTALL_YUNOHOST ] ; then
   chroot_deb $TARGET_DIR "cd /tmp/install_script && ./install_yunohost -a -d ${INSTALL_YUNOHOST_DIST}"
 
   if [ "${INSTALL_YUNOHOST_DIST}" != stable ]; then
-    chroot_deb $TARGET_DIR "rmdir /run/systemd/system/ /run/systemd/"
+    chroot_deb $TARGET_DIR "rmdir /run/systemd/system/ /run/systemd/ 2> /dev/null || true"
   fi
 fi
 
