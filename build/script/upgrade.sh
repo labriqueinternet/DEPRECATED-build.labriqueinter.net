b9f151003bf68d0a6bb64a32dd433da80bf2b92f
Émile Morel <github@bleuchtang.fr>
2016-05-08 05:29:56 +0200

    update the upgrade script

--- build/script/upgrade.sh
+++ build/script/upgrade.sh
@@ -1,50 +1,36 @@
-#!/bin/sh
+#!/bin/bash 
 
+set -xe
 
-echo 'Remove pinning from testing'
-rm -f /etc/apt/sources.list.d/testing.list
-rm -f /etc/apt/preferences.d/kernel-testing
+KERNEL_VERSION=$(uname -r)
 
-echo 'Install pinning file (/etc/apt/preferences.d/kernel-backports)'
-cat <<EOT > ${TARGET_DIR}/etc/apt/preferences.d/kernel-backports
-Package: linux-image*
-Pin: release a=jessie-backports
-Pin-Priority: 990
+if dpkg -l | grep -q linux-image; then
 
-Package: u-boot*
-Pin: release a=jessie-backports
-Pin-Priority: 990
+  echo $KERNEL_VERSION | grep -q 3.16 
+  
+  if [ $? -eq 0 ] ; then 
 
-Package: flash-kernel*
-Pin: release a=jessie-backports
-Pin-Priority: 990
+   echo "Nothing to do, quit"
+   exit 1
 
-Package: *
-Pin: release a=jessie-backports
-Pin-Priority: 50
-EOT
+  else
 
-echo 'Install Debian backports sources (/etc/apt/sources.list.d/backports.list)'
-cat <<EOF > /etc/apt/sources.list.d/backports.list
-deb http://ftp.fr.debian.org/debian jessie-backports main
-EOF
+    rm -f /etc/apt/sources.list.d/{testing,backport}.list
+    rm -f /etc/apt/preferences.d/kernel-{backports,testing}
+    echo "linux-image-$KERNEL_VERSION linux-image-$KERNEL_VERSION/prerm/removing-running-kernel-$KERNEL_VERSION boolean false" | debconf-set-selections
+    DEBIAN_FRONTEND=noninteractive apt-get remove -y --force-yes --purge linux-image-4* flash-kernel u-boot-tools u-boot-sunxi
 
-echo 'Configure flash-kernel'
-if [ -f "/etc/crypttab" ]; then
-  echo 'LINUX_KERNEL_CMDLINE="console=ttyS1 hdmi.audio=EDID:0 disp.screen0_output_mode=EDID:1280x720p60 root=/dev/mapper/root cryptopts=target=root,source=/dev/mmcblk0p2,cipher=aes-xts-plain64,size=256,hash=sha1 rootwait sunxi_ve_mem_reserve=0 sunxi_g2d_mem_reserve=0 sunxi_no_mali_mem_reserve sunxi_fb_mem_reserve=0 panic=10 loglevel=6 consoleblank=0"' > /etc/default/flash-kernel
+  fi
+
 else
-  echo 'LINUX_KERNEL_CMDLINE="console=tty0 hdmi.audio=EDID:0 disp.screen0_output_mode=EDID:1280x720p60 root=/dev/mmcblk0p1 rootwait sunxi_ve_mem_reserve=0 sunxi_g2d_mem_reserve=0 sunxi_no_mali_mem_reserve sunxi_fb_mem_reserve=0 panic=10 loglevel=6 consoleblank=0"' >  /etc/default/flash-kernel
+
+  echo 'LINUX_KERNEL_CMDLINE="console=ttyS1 hdmi.audio=EDID:0 disp.screen0_output_mode=EDID:1280x720p60 root=/dev/mmcblk0p1 rootwait sunxi_ve_mem_reserve=0 sunxi_g2d_mem_reserve=0 sunxi_no_mali_mem_reserve sunxi_fb_mem_reserve=0 panic=10 loglevel=6 consoleblank=0"' > $TARGET_DIR/etc/default/flash-kernel
+
 fi
 
-if grep -q testing /etc/apt/sources.list ; then
-  echo "##################################################################"
-  echo "# Warning; you probably doesn't want yunohost testing repository #"
-  echo "##################################################################"
-  echo "line:"
-  grep testing /etc/apt/sources.list
-fi
-echo 'Now you can run (answer no, and type enter twice):'
-echo 'apt-get update'
-echo 'apt-get dist-upgrade'
-echo 'apt-get install linux-image-armmp flash-kernel u-boot-sunxi u-boot-tools'
-echo 'apt-get clean'
+apt-get clean
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt install -y --force-yes linux-image-armmp flash-kernel u-boot-sunxi u-boot-tools
+update-initramfs -k all -u
+
+exit 0
