1a4c29c8efe8c7b6891f9d81467fec288bd26ab2
Alpheb <alpheb@gmail.com>
2016-07-05 00:20:39 +0200

    Encrypted rootfs: Avoid bugs on malformed querystring

--- build/script/initramfs/www/cgi-bin/post.sh
+++ build/script/initramfs/www/cgi-bin/post.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
 read QUERY_STRING
-eval $(echo "$QUERY_STRING" | awk -F'&' '{for(i=1; i <= NF; i++) { print $i }}')
 
-echo -n $(httpd -d $passphrase) > /lib/cryptsetup/passfifo
+# We now split the query string at '&' then each part at '='
+# We look for the X in a=b&passphrase=X&c=d
+passphrase_urlenc=$(echo "$QUERY_STRING" | awk -F'&'   \
+  '{                                                   \
+    for(i=1; i <= NF; i++) {                           \
+      n = split($i, array, "=");                       \
+      if(n == 2 && index(array[1], "passphrase")) {    \
+        print array[2];                                \
+        break;                                         \
+      }                                                \
+    }                                                  \
+  }'                                                   \
+)
+
+echo -n $(httpd -d "$passphrase_urlenc") > /lib/cryptsetup/passfifo
 
 for i in $(seq 30); do
   sleep 1
