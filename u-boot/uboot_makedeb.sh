2331218f786eaedf7363aa1aea0c75ff060ed709
Julien Vaubourg <julien@vaubourg.com>
2016-11-17 19:06:36 +0100

    Add install documentation

--- u-boot/uboot_makedeb.sh
+++ u-boot/uboot_makedeb.sh
@@ -3,17 +3,18 @@
 set -eux
 
 # u-boot-sunxi Debian version (in stable or testing)
-boot_version="2016.09+dfsg1-2"
+uboot_version="2016.09+dfsg1-2"
 
 working_dir=$PWD
 
+# You must be directly in u-boot/
 if ! [ -d "${working_dir}/patches/" ]; then
   echo "missing patches/ directory" >&2
   exit 1
 fi
 
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y --force-yes fakeroot devscripts ncurses-dev u-boot-tools build-essential
+DEBIAN_FRONTEND=noninteractive apt-get install -y --force-yes fakeroot devscripts ncurses-dev build-essential
 
 tmp_dir=$(mktemp -dp $working_dir uboot_makedeb-tmpXXXXX)
 pushd $tmp_dir &> /dev/null
@@ -31,26 +32,30 @@ pushd $source_dir &> /dev/null
 
 export NAME="La Brique Internet"
 export DEBEMAIL="discussions@listes.labriqueinter.net"
-increment_opt="-n"
 
 debpatches_dir="A20-OLinuXino-labriqueinternet"
 mkdir -p "debian/patches/${debpatches_dir}/"
 
+dch -n "Remove all targets, excepting A20-OLinuXino-Lime(2)."
+
+# Applies additional patches on the upstream sources
 while read p; do
   cp "${working_dir}/patches/${p}" "debian/patches/${debpatches_dir}/"
   echo "${debpatches_dir}/${p}" >> debian/patches/series
   patch -p1 "debian/patches/${debpatches_dir}/${p}"
 
+  # Completes the deb changelog file with the additional changes
   while read c; do
-    dch $increment_opt "u-boot-sunxi > A20-OLinuXino: ${c}"
-    increment_opt=
+    dch "u-boot-sunxi/A20-OLinuXino-Lime(2): ${c}"
   done < "${working_dir}/patches/${p}.changelog"
 done < "${working_dir}/patches/series"
 
+# Only Sunxi Lime/Lime2 for us
+sed '/[[:space:]]\+A20-OLinuXino-Lime.\?[[:space:]]\+/!d' -i debian/targets
 export DEB_HOST_ARCH=armhf
 time fakeroot debian/rules binary |& tee "${working_dir}/uboot_makedeb.log"
 
-find -maxdepth 1 -name '*sunxi*.deb' -exec mv {} "${working_dir}/" \;
+find -maxdepth 1 -name 'u-boot-sunxi*.deb' -exec mv {} "${working_dir}/" \;
 
 cd ~2
 rm -rf $tmp_dir
