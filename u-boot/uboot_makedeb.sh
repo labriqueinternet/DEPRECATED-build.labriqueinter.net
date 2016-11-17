126cf123c0271f13ac957ddd13a4f7d4d581fd5f
Julien Vaubourg <julien@vaubourg.com>
2016-11-17 14:41:58 +0100

    Update uboot makedeb script

--- u-boot/uboot_makedeb.sh
+++ u-boot/uboot_makedeb.sh
@@ -2,12 +2,12 @@
 
 set -eux
 
-# u-boot-sunxi Debian version (in stable or testing)
-uboot_version="2016.09+dfsg1-2"
+u-boot-sunxi Debian version (in stable or testing)
+boot_version="2016.09+dfsg1-2"
 
-build_dir=$PWD
+working_dir=$PWD
 
-if ! [ -d "${build_dir}/patches/" ]; then
+if ! [ -d "${working_dir}/patches/" ]; then
   echo "missing patches/ directory" >&2
   exit 1
 fi
@@ -15,7 +15,7 @@ fi
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -y --force-yes fakeroot devscripts ncurses-dev u-boot-tools build-essential
 
-tmp_dir=$(mktemp -dp $build_dir uboot_makedeb-tmpXXXXX)
+tmp_dir=$(mktemp -dp $working_dir uboot_makedeb-tmpXXXXX)
 pushd $tmp_dir &> /dev/null
 
 if ! grep -q deb-src /etc/apt/sources.list; then
@@ -33,25 +33,24 @@ export NAME="La Brique Internet"
 export DEBEMAIL="discussions@listes.labriqueinter.net"
 increment_opt="-n"
 
-for i in 1 2; do
-  mkdir -p "debian/patches/A20-OLinuXino-LIME${i}/"
+debpatches_dir="A20-OLinuXino-labriqueinternet"
+mkdir -p "debian/patches/${debpatches_dir}/"
 
-  while read p; do
-    cp "${build_dir}/patches/LIME${i}/${p}" "debian/patches/A20-OLinuXino-LIME${i}/"
-    echo "A20-OLinuXino-LIME${i}/${p}" >> debian/patches/series
-    patch -p1 "debian/patches/A20-OLinuXino-LIME${i}/${p}"
-  
-    while read c; do
-      dch $increment_opt "u-boot-sunxi > LIME${i}: ${c}"
-      increment_opt=
-    done < "${build_dir}/patches/LIME${i}/${p}.changelog"
-  done < "${build_dir}/patches/LIME${i}/series"
-done
+while read p; do
+  cp "${working_dir}/patches/${p}" "debian/patches/${debpatches_dir}/"
+  echo "${debpatches_dir}/${p}" >> debian/patches/series
+  patch -p1 "debian/patches/${debpatches_dir}/${p}"
+
+  while read c; do
+    dch $increment_opt "u-boot-sunxi > A20-OLinuXino: ${c}"
+    increment_opt=
+  done < "${working_dir}/patches/${p}.changelog"
+done < "${working_dir}/patches/series"
 
 export DEB_HOST_ARCH=armhf
-time fakeroot debian/rules binary |& tee "${build_dir}/uboot_makedeb.log"
+time fakeroot debian/rules binary |& tee "${working_dir}/uboot_makedeb.log"
 
-mv ./*sunxi*.deb "${build_dir}/"
+find -maxdepth 1 -name '*sunxi*.deb' -exec mv {} "${working_dir}/" \;
 
 cd ~2
 rm -rf $tmp_dir
