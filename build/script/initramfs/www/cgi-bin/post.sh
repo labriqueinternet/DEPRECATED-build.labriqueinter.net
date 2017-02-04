c054d003f447af260cecf9622439a8ef4c6836d1
Julien Vaubourg <julien@vaubourg.com>
2017-02-04 21:52:00 +0100

    Fix initramfs

--- build/script/initramfs/www/cgi-bin/post.sh
+++ build/script/initramfs/www/cgi-bin/post.sh
@@ -3,20 +3,19 @@
 read QUERY_STRING
 passphrase=$(echo "${QUERY_STRING}" | sed 's/.*passphrase=\([^&]\+\).*/\1/')
 echo -n $(httpd -d "${passphrase}") > /lib/cryptsetup/passfifo
-sleep 5
 
 echo -e "Content-type: text/plain\n"
 status=unknown
 
 while [ $status = unknown ]; do
-  sleep 1
-
-  if [ -b /dev/mapper/root ]; then
+  if [ ! -f /bin/sleep ]; then
     status=success
 
   elif ps aux | grep -q [c]ryptsetup/askpass; then
     status=failed
   fi
+
+  sleep 1
 done
 
 echo -n $status
