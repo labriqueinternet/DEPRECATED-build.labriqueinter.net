00a4c1d10d0618965a89022f9ac2bb5119d1a084
HugoPoi <hugo.poissonnet@gmail.com>
2020-05-13 23:06:22 +0200

    refactor(builder): move dependencies install command in internetcube setup

--- yunocube.sh
+++ yunocube.sh
@@ -2,17 +2,26 @@
 set -e
 set -x
 
+OVERLAY_PATH=/tmp/overlay/internetcube
+
 InstallInternetCubeServices(){
 
-# Install hypercube service
-mkdir -p /var/log/hypercube
-install -m 755 -o root -g root ${OVERLAY_PATH}/hypercube/hypercube.sh /usr/local/bin/
-install -m 444 -o root -g root ${OVERLAY_PATH}/hypercube/hypercube.service /etc/systemd/system/
-install -m 444 -o root -g root ${OVERLAY_PATH}/hypercube/install.html /var/log/hypercube/
+  # Install InternetCube dependencies usb detection, hotspot, vpnclient, roundcube
+  apt-get install -o Dpkg::Options::='--force-confold' -y \
+    file udisks2 udiskie ntfs-3g jq \
+    php7.0-fpm sipcalc hostapd iptables iw dnsmasq firmware-linux-free \
+    sipcalc dnsutils openvpn curl fake-hwclock \
+    php-cli php-common php-intl php-json php-mcrypt php-pear php-auth-sasl php-mail-mime php-patchwork-utf8 php-net-smtp php-net-socket php-net-ldap2 php-net-ldap3 php-zip php-gd php-mbstring php-curl
 
-# Enable hypercube service
-# TODO use systemctl for doing this
-ln -f -s '/etc/systemd/system/hypercube.service' /etc/systemd/system/multi-user.target.wants/hypercube.service
+  # Install hypercube service
+  mkdir -p /var/log/hypercube
+  install -m 755 -o root -g root ${OVERLAY_PATH}/hypercube.sh /usr/local/bin/
+  install -m 444 -o root -g root ${OVERLAY_PATH}/hypercube.service /etc/systemd/system/
+  install -m 444 -o root -g root ${OVERLAY_PATH}/install.html /var/log/hypercube/
+
+  # Enable hypercube service
+  # TODO use systemctl for doing this
+  ln -f -s '/etc/systemd/system/hypercube.service' /etc/systemd/system/multi-user.target.wants/hypercube.service
 
 }
 
